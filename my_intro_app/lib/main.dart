9ba088d9dec3701c3cc62d0006075831234e6272
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-06-12 13:30:12 +0500

    Lecture 4
    My intro app - final class work

--- my_intro_app/lib/main.dart
+++ my_intro_app/lib/main.dart
@@ -15,10 +15,63 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 60.0,
                 backgroundImage: AssetImage("images/jahanzeb.png"),
+              ),
+              Text(
+                "Jahanzeb Naeem",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade900,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: "Source Sans Pro",
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade200,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 5.0,
+                  horizontal: 100.0,
+                ),
+                child: Divider(
+                  thickness: 5,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 5.0,
+                  horizontal: 20.0,
+                ),
+                elevation: 5.0,
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 30.0,
+                      color: Colors.teal.shade700,
+                    ),
+                    title: Text(
+                      "+92 123 456 7890",
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
