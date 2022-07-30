0cdc3126584c4876b7a43eeae2817d5c81e6c535
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-07-30 22:11:36 +0500

    Lecture 13
    Implementing animations to the project.

--- flash_chat_app/lib/screens/welcome_screen.dart
+++ flash_chat_app/lib/screens/welcome_screen.dart
@@ -1,18 +1,73 @@
-import 'package:flash_chat_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'login_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
-  static const String id = 'Login_screen';
+  static const String id = 'welcome_screen';
 
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+      // upperBound: 100.0,
+    );
+
+    // animation = CurvedAnimation(
+    //   parent: controller,
+    //   // curve: Curves.decelerate,
+    //   curve: Curves.easeIn,
+    // );
+
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white,
+    ).animate(controller);
+
+    // controller.forward();
+    // controller.reverse(from: 1.0);
+    controller.forward();
+
+    // animation.addStatusListener((status) {
+    //   // print(status);
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    // });
+
+    controller.addListener(() {
+      setState(() {});
+      // print(controller.value);
+      print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.white,
+      // backgroundColor: Colors.red.withOpacity(controller.value),
+      // backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -21,16 +76,35 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
-                ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    // height: 60.0,
+                    // height: controller.value,
+                    // height: animation.value * 100,
+                    height: 60.0,
                   ),
+                ),
+                // Text(
+                //   //   // 'Flash Chat',
+                //   //   // '${controller.value.toInt()}%',
+                //   'Flash Chat',
+                //   style: TextStyle(
+                //     fontSize: 45.0,
+                //     fontWeight: FontWeight.w900,
+                //   ),
+                // ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Flash Chat',
+                      textStyle: TextStyle(
+                        fontSize: 45.0,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
